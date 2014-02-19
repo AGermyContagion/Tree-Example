1e780518152820b1c131f75eb4dc25ac62a90091
jerewarr <jerewarr@indiana.edu>
2014-02-19 02:29:58 -0500

    Finished Tree Ordering
    Pre, In, and Post ordering working correctly.

--- TreeExample/src/BinaryTree.java
+++ TreeExample/src/BinaryTree.java
@@ -9,7 +9,6 @@ public class BinaryTree {
 		} else {
 			BinaryNode<Integer, String> focusNode = root;
 			BinaryNode<Integer, String> parent;
-			int i = 0;
 			while (true) {
 				parent = focusNode;
 				if (key < focusNode.getKey()) {
@@ -29,26 +28,74 @@ public class BinaryTree {
 		}
 	}
 	
+	public String inorderTraverse() {
+		return inorderTraverse(root, 'b');
+	}
+	
 	/**
 	 * Traverse the tree and retern a string of the traverse
 	 * @param focusNode
 	 * @return String with the tree traversed by inorder
 	 */
-	public String inorderTraverse(BinaryNode<Integer, String> focusNode) {
-		String x = "";
-		if (focusNode.getLeft() != null) inorderTraverse(focusNode.getLeft());
-		x += focusNode;
-		if (focusNode.getRight() != null) inorderTraverse(focusNode.getRight());
-		return x;
+	public String inorderTraverse(BinaryNode<Integer, String> focusNode, char output) {
+		String result = "";
+		if (focusNode.getLeft() != null) result += inorderTraverse(focusNode.getLeft(), output);
+		
+		if (output == 'k') result += focusNode.getKey();
+		else if (output == 'v') result += focusNode.getValue();
+		else result += ("(" + focusNode.getKey() + ":" + focusNode.getValue() + ")");
+		
+		if (focusNode.getRight() != null) result += inorderTraverse(focusNode.getRight(), output);
+		return result;
+	}
+	
+	public String preorderTraverse() {
+		return preorderTraverse(root, 'b');
+	}
+	
+	public String preorderTraverse(BinaryNode<Integer, String> focusNode, char output) {
+		String result = "";
+		if (output == 'k') result += focusNode.getKey();
+		else if (output == 'v') result += focusNode.getValue();
+		else result += ("(" + focusNode.getKey() + ":" + focusNode.getValue() + ")");
+		
+		if (focusNode.getLeft() != null)  result += preorderTraverse(focusNode.getLeft(), output);
+		
+		if (focusNode.getRight() != null) result += preorderTraverse(focusNode.getRight(), output);
+		return result;
+	}
+	
+	public String postorderTraverse() {
+		return postorderTraverse(root, 'b');
+	}
+	
+	public String postorderTraverse(BinaryNode<Integer, String> focusNode, char output) {
+		String result = "";
+		
+		if (focusNode.getLeft() != null)  result += postorderTraverse(focusNode.getLeft(), output);
+		
+		if (focusNode.getRight() != null) result += postorderTraverse(focusNode.getRight(), output);
+		
+		if (output == 'k') result += focusNode.getKey();
+		else if (output == 'v') result += focusNode.getValue();
+		else result += ("(" + focusNode.getKey() + ":" + focusNode.getValue() + ")");
+		return result;
 	}
 	
 	/**
 	 * Overrides the toString() method
-	 * Returns the tree inorder by default
+	 * Returns the Binary Tree Structure as a String
 	 */
 	@Override public String toString() {
-		return inorderTraverse(root);
+		return toStringHelper(root);
 	}
 	
+	private String toStringHelper(BinaryNode<Integer, String> focusNode) {
+		String result = "";
+		result += focusNode;
+		if (focusNode.getLeft() != null) toStringHelper(focusNode.getLeft());
+		if (focusNode.getRight() != null) toStringHelper(focusNode.getRight());
+		return result;
+	}
 	
 }
