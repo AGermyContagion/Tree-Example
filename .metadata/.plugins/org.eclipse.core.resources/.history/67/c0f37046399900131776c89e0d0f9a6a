a464852e8b5d2c4ea4e8008e56c7fa4462a2c8e3
jerewarr <jerewarr@indiana.edu>
2014-02-19 02:42:05 -0500

    Cosmetic Changes to BinaryNode Class

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/67/c0f37046399900131776c89e0d0f9a6a
@@ -0,0 +1,72 @@
+
+public class BinaryNode<Key, V> {
+	private Key key;
+	private V value;
+	private BinaryNode<Key, V> left;
+	private BinaryNode<Key, V> right;
+	
+	@Override public String toString() {
+		return "(k:" + this.key + " v:" + this.value + " {l: " + this.left + " r: " + this.right + "})";
+	}
+	
+	public BinaryNode(Key k, V value) {
+		this.key = k;
+		this.value = value;
+		this.left = this.right = null;
+	}
+	
+	public void setKey(Key k) {
+		this.key = k;
+	}
+	
+	public Key getKey() {
+		return this.key;
+	}
+	
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+	public V getValue() {
+		return this.value;
+	}
+	
+	public boolean isLeaf() {
+		if ((this.left == null) && (this.right == null)) return true;
+		else return false;
+	}
+	
+	public void setLeft(BinaryNode<Key, V> left) {
+		this.left = left;
+	}
+	
+	public BinaryNode<Key, V> getLeft() {
+		return this.left;
+	}
+	
+	public boolean hasLeft() {
+		if (this.left == null) return false;
+		else return true;
+	}
+	
+	public void removeLeft() {
+		this.left = null;
+	}
+	
+	public void setRight(BinaryNode<Key, V> right) {
+		this.right = right;
+	}
+	
+	public BinaryNode<Key, V> getRight() {
+		return this.right;
+	}
+	
+	public boolean hasRight() {
+		if (this.right == null) return false;
+		else return true;
+	}
+	
+	public void removeRight() {
+		this.right = null;
+	}
+}
--- TreeExample/src/BinaryNode.java
+++ TreeExample/src/BinaryNode.java
@@ -5,9 +5,7 @@ public class BinaryNode<Key, V> {
 	private BinaryNode<Key, V> left;
 	private BinaryNode<Key, V> right;
 	
-	@Override public String toString() {
-		return "(k:" + this.key + " v:" + this.value + " {l: " + this.left + " r: " + this.right + "})";
-	}
+	@Override public String toString() { return "(k:" + this.key + " v:" + this.value + " {l: " + this.left + " r: " + this.right + "})"; }
 	
 	public BinaryNode(Key k, V value) {
 		this.key = k;
@@ -15,58 +13,38 @@ public class BinaryNode<Key, V> {
 		this.left = this.right = null;
 	}
 	
-	public void setKey(Key k) {
-		this.key = k;
-	}
+	public void setKey(Key k) { this.key = k; }
 	
-	public Key getKey() {
-		return this.key;
-	}
+	public Key getKey() { return this.key; }
 	
-	public void setValue(V value) {
-		this.value = value;
-	}
+	public void setValue(V value) { this.value = value; }
 	
-	public V getValue() {
-		return this.value;
-	}
+	public V getValue() { return this.value; }
 	
 	public boolean isLeaf() {
 		if ((this.left == null) && (this.right == null)) return true;
 		else return false;
 	}
 	
-	public void setLeft(BinaryNode<Key, V> left) {
-		this.left = left;
-	}
+	public void setLeft(BinaryNode<Key, V> left) { this.left = left; }
 	
-	public BinaryNode<Key, V> getLeft() {
-		return this.left;
-	}
+	public BinaryNode<Key, V> getLeft() { return this.left; }
 	
 	public boolean hasLeft() {
 		if (this.left == null) return false;
 		else return true;
 	}
 	
-	public void removeLeft() {
-		this.left = null;
-	}
+	public void removeLeft() { this.left = null; }
 	
-	public void setRight(BinaryNode<Key, V> right) {
-		this.right = right;
-	}
+	public void setRight(BinaryNode<Key, V> right) { this.right = right; }
 	
-	public BinaryNode<Key, V> getRight() {
-		return this.right;
-	}
+	public BinaryNode<Key, V> getRight() { return this.right; }
 	
 	public boolean hasRight() {
 		if (this.right == null) return false;
 		else return true;
 	}
 	
-	public void removeRight() {
-		this.right = null;
-	}
+	public void removeRight() { this.right = null; }
 }
