bfcf27455dc815ccbce997746f6f2f7a7f4eb534
jerewarr <jerewarr@indiana.edu>
2014-02-25 22:02:22 -0500

    Completed Week 6 Homework part 3

--- TreeExample/src/Runner.java
+++ TreeExample/src/Runner.java
@@ -1,36 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		BinaryTree<String, Integer> tree = new BinaryTree<String, Integer>();
-		//long startTime;
-		//long endTime;
+		ArrayList<BinaryNode<Integer, Character>> nodes = new ArrayList<BinaryNode<Integer, Character>>();
 		
-		tree.addNode("A", 15);
-		tree.addNode("B", 20);
-		tree.addNode("C", 25);
-		tree.addNode("D", 18);
-		tree.addNode("E", 16);
-		tree.addNode("F", 5);
-		tree.addNode("E", 16);
-		tree.addNode("G", 7);
-		tree.addNode("G", 7);
+		nodes.add(new BinaryNode<Integer, Character>(50, 'A'));
+		nodes.add(new BinaryNode<Integer, Character>(35, 'T'));
+		nodes.add(new BinaryNode<Integer, Character>(10, 'C'));
+		nodes.add(new BinaryNode<Integer, Character>(5, 'G'));
 		
-		System.out.println(tree);
-		System.out.println(tree.getNumberOfNodes());
-		System.out.println(tree.inorderTraverse('k'));
-		System.out.println(tree.preorderTraverse('k'));
-		System.out.println(tree.postorderTraverse('k'));
+	}
+	
+	public static BinaryTree<Integer, Character> buildHuffmanTree(ArrayList<BinaryNode<Integer, Character>> ns) {
+		ArrayList<BinaryTree<Integer, Character>> parts = new ArrayList<BinaryTree<Integer, Character>>();
+		ArrayList<BinaryNode<Integer, Character>> nsCopy = new ArrayList<BinaryNode<Integer, Character>>(ns);
+		ArrayList<BinaryNode<Integer, Character>> result = new ArrayList<BinaryNode<Integer, Character>>();
+		int current = 0;
 		
-		System.out.println(tree.inorderTraverse('v'));
-		System.out.println(tree.preorderTraverse('v'));
-		System.out.println(tree.postorderTraverse('v'));
-		
-		System.out.println(tree.inorderTraverse());
-		System.out.println(tree.preorderTraverse());
-		System.out.println(tree.postorderTraverse());
-		tree.clearTree();
-		System.out.println(tree);
+		while (result.size() < nsCopy.size()) {
+			if (result.isEmpty()) {
+				result.add(nsCopy.get(0));
+				nsCopy.remove(0);
+				current++;
+			} else {
+				for (int i = 0; i < )
+				if (result.get(current) < nsCopy.0) {
+					
+				}
+			}
+		}
 		
 	}
 }
